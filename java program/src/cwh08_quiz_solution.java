ac3841a7733c7dfa352d6b75588efd5e40364613
Musa6630 <musabbirrockzz123@gmail.com>
2021-08-26 16:11:37 +0530

    name changes

---
+++ java program/src/cwh08_quiz_solution.java
@@ -0,0 +1,20 @@
+public class cwh08_quiz_solution {
+    public static void main(String[] args) {
+        //Increment operators
+        int a = 5;
+        //int c = a++;
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+
+        //Decrement operators
+        int b = 7;
+        System.out.println(b--);
+        System.out.println(--b);
+        System.out.println(b--);
+        System.out.println(b);
+    }
+}
