f32b637355fa06a11e81c3b82720900f0772d9ff
Musa6630 <musabbirrockzz123@gmail.com>
2021-08-26 19:16:55 +0530

    name changes

--- java program/src/cwh09_strings.java
+++ java program/src/cwh09_strings.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class cwh09_strings {
     public static void main(String[] args) {
+        //String name = new String("my name is musabbir");
+        //System.out.println(name);
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter your name here: ");
+        String name = input.nextLine();
+        System.out.println("hiie " + name + " Have a good day!");
+        String name2 = "Harry";
+        String name1 = "harry bhai";
+        System.out.println("hiie " + name2 + " have a good day!");
     }
 }
